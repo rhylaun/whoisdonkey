558a5fda96bbf947df4b4f56bb42e69c0b6d7036
rhylaun <sodevision@yandex.ru>
2017-06-18 20:13:28 +0500

    добавление скрипта для отображения меню в игровой сцене по кнопке ESC

---
+++ UnityClient/WhoIsDonkey/Assets/Scripts/GameMenuShowScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class GameMenuShowScript : MonoBehaviour
+{
+	public GameObject GameMenu;
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			GameMenu.SetActive(!GameMenu.activeSelf);
+		}
+	}
+}

5af8127d640ca62b64c03a718e315751e4b06a29
rhylaun <sodevision@yandex.ru>
2017-07-02 20:32:21 +0500

    добавил звук в меню и прикрутил регулировку

---
+++ UnityClient/WhoIsDonkey/Assets/Scripts/VolumeCalculatorScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeCalculatorScript : MonoBehaviour
+{
+	public GameObject MasterSlider;
+	public GameObject MusicSlider;
+	public GameObject EffectSlider;
+
+	public AudioSource MusicSource;
+	public AudioSource EffectSource;
+
+	private Slider _musicSlider;
+	private Slider _masterSlider;
+	private Slider _effectSlider;
+
+	public void Start()
+	{
+		_musicSlider = MusicSlider.GetComponent<Slider>();
+		_masterSlider = MasterSlider.GetComponent<Slider>();
+		_effectSlider = EffectSlider.GetComponent<Slider>();
+	}
+
+	public void AdjustMasterVolume()
+	{
+		MusicSource.volume = _musicSlider.value * _masterSlider.value;
+		EffectSource.volume = _effectSlider.value * _masterSlider.value;
+	}
+
+	public void AdjustMusicVolume()
+	{
+		MusicSource.volume = _musicSlider.value * _masterSlider.value;
+	}
+
+	public void AdjustEffectVolume()
+	{
+		EffectSource.volume = _effectSlider.value * _masterSlider.value;
+	}
+}

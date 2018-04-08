1552555954dd1cc2f8811c9edf8129663e2ed80c
rhylaun <sodevision@yandex.ru>
2018-04-08 10:48:16 +0500

    Исправил ошибку с несколькими вхождениями в лобби когда игрок создает лобби

--- DonkeyServer/CommandProcessors/CreateLobbyCommandProcessor.cs
+++ DonkeyServer/CommandProcessors/CreateLobbyCommandProcessor.cs
@@ -5,30 +5,41 @@ using Donkey.Common.Answers;
 namespace Donkey.Server.CommandProcessors
 {
 	[CommandProcessorInfo(CommandType = CommandType.CreateLobby)]
-    public class CreateLobbyCommandProcessor : BaseCommandProcessor
-    {
-        public CreateLobbyCommandProcessor(ClientCommand command)
-            : base(command)
-        {
-        }
+	public class CreateLobbyCommandProcessor : BaseCommandProcessor
+	{
+		public CreateLobbyCommandProcessor(ClientCommand command)
+			: base(command)
+		{
+		}
 
-        protected override bool Process(GameServer server)
-        {
-            var result = true;
-            var command = (CreateLobbyCommand)Command;
-            try
-            {
-                var lobby = server.CreateLobby(command.LobbyName);
-                var player = server.GetPlayer(command.AuthData);
-                lobby.AddPlayer(player);
-            }
-            catch (GameServerException)
-            {
-                result = false;
-            }
-            Answer = new CreateLobbyAnswer(result);
+		protected override bool Process(GameServer server)
+		{
+			var result = true;
+			var command = (CreateLobbyCommand)Command;
+			try
+			{
+				var lobbies = server.GetLobbies();
+				var alreadyExist = lobbies.Contains(command.LobbyName);
+				Lobby lobby;
+				if (!alreadyExist)
+				{
+					lobby = server.CreateLobby(command.LobbyName);
+				}
+				else
+				{
+					lobby = server.GetLobby(command.LobbyName);
+				}
 
-            return result;
-        }
-    }
+				var player = server.GetPlayer(command.AuthData);
+				lobby.AddPlayer(player);
+			}
+			catch (GameServerException)
+			{
+				result = false;
+			}
+			Answer = new CreateLobbyAnswer(result);
+
+			return result;
+		}
+	}
 }

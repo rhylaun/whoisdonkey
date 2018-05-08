15ac607264c614dec528bbea928a9c8cee03b3d1
Alexander Khalturin <sodevision@yandex.ru>
2018-05-09 00:26:50 +0500

    Сохранение настроек (#2)
    * Добавил меню на экран логина,теперь можно устанавливать настройки звука
    перед логином в игру
    
    * Сделал сохранение и загрузку настроек

---
+++ UnityClient/WhoIsDonkey/Assets/Scripts/LoginMenuKeybindsBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoginMenuKeybindsBehaviour : MonoBehaviour
+{
+	public GameObject Menu;
+	public GameObject LoginForm;
+
+	private bool firstEnter = true;
+
+	void Update()
+	{
+		if (Input.anyKeyDown && firstEnter)
+		{
+			firstEnter = false;
+			return;
+		}
+
+		if (!Input.GetKeyUp(KeyCode.Escape))
+			return;
+
+		SwapMenu();
+	}
+
+	private void SwapMenu()
+	{
+		var isActive = Menu.activeSelf;
+		Menu.SetActive(!isActive);
+		LoginForm.SetActive(isActive);
+	}
+}
